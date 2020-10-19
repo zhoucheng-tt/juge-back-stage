15dce9ccfea35f24209fdb875942a3b4aa2b2217
hj327 <hjsweets@163.com>
2020-10-19 15:54:12 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-19 15:52:46
+ * @LastEditTime: 2020-10-19 15:54:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \ting-car\src\views\About.vue
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div></div>
   </div>
 </template>
