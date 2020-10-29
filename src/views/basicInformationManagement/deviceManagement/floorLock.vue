6fb49bf7b483fdad3ee5f126f6bb06aab1d17e8e
shaoqingyang0930 <52151837+shaoqingyang0930@users.noreply.github.com>
2020-10-29 09:40:03 +0800

    邵青阳新建设备管理-地锁和进出口摄像头管理

---
+++ src/views/basicInformationManagement/deviceManagement/floorLock.vue
@@ -0,0 +1,14 @@
+<!--
+    地锁
+ * @Author: 邵青阳
+ * @Date: 2020-10-29 09:32:31
+ * @LastEditTime: 2020-10-29 09:33:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \g524-comprehensive-displayd:\TingCar\src\views\basicInformationManagement\deviceManagement\floorLock.vue
+-->
+<template>
+    <div>
+        地锁
+    </div>
+</template>
