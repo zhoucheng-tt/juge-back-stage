fa5e440a4ee462b6dc424c2fb3d118203b1cad77
shaoqingyang0930 <52151837+shaoqingyang0930@users.noreply.github.com>
2020-11-09 10:15:16 +0800

    邵青阳提交相关确实页面

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-24 14:16:45
- * @LastEditTime: 2020-10-29 13:37:08
+ * @LastEditTime: 2020-11-09 10:14:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \g524-comprehensive-displayd:\company Environment\tcxt-web\src\axios\index.js
@@ -12,30 +12,46 @@ import * as ysParking from './ysParking/ysParking.js'
 import * as homePage from './homePage/homePage.js'
 // 设备管理接口
 import * as deviceManagement from './deviceManagement/deviceManagement.js'
+// 报表分析
+import * as reportAnalysis from './reportAnalysis/reportAnalysis.js'
+// 系统管理接口
+import * as systemUser from './systemUser/systemUser.js'
 
-const install = function(Vue){
-	if(install.installed){
+const install = function (Vue) {
+	if (install.installed) {
 		return install.installed = true
 	}
-	Object.defineProperties(Vue.prototype,{
-		$ysParking:{
-			get(){
+	Object.defineProperties(Vue.prototype, {
+		$ysParking: {
+			get() {
 				return ysParking
 			}
 		},
 		// 首页接口  homePage
-		$homePage:{
-			get(){
+		$homePage: {
+			get() {
 				return homePage
 			}
 		},
-	//	设备管理页面接口
-		$deviceManagement:{
-			get(){
+		//	设备管理页面接口
+		$deviceManagement: {
+			get() {
 				return deviceManagement
 			}
-		}
+		},
+		// 报表分析接口
+		$reportAnalysis: {
+			get() {
+				return reportAnalysis
+			}
+		},
+		// 系统管理接口  
+		$systemUser: {
+			get() {
+				return systemUser
+			}
+		},
 	})
 }
 
-export default {install}
+export default { install }
