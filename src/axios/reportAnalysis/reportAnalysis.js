753a76185a2ade0a4a5bfa0715514f17a70dc31b
任治厅 <1330723194@qq.com>
2020-11-09 13:57:58 +0800

    报表分析-支付台账分析-接口调试

---
+++ src/axios/reportAnalysis/reportAnalysis.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+//支付台账分析列表查询
+export function queryAccountStatisList(param) {
+    return request(
+        {
+            url: 'accountStatisFunc/queryAccountStatisList',
+            method: 'post',
+            data: param
+        }
+    )
+}
+
+//缴费金额按月分析
+export function trendAnalysis(param) {
+    return request(
+        {
+            url: 'accountStatisFunc/trendAnalysis',
+            method: 'post',
+            data: param
+        }
+    )
+}
