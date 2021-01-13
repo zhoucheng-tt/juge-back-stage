1aaad681f03067cb2faabd09733ee956d448349a
任治厅 <1330723194@qq.com>
2021-01-13 16:32:54 +0800

    任 20210113

--- src/axios/realTimeMonitor/realTimeMonitor.js
+++ src/axios/realTimeMonitor/realTimeMonitor.js
@@ -11,6 +11,7 @@ export function queryWashData(param) {
         }
     )
 }
+
 //洗车收入分析
 export function queryWashCount(param) {
     return request(
@@ -28,6 +29,17 @@ export function queryWashEarn(param) {
     return request(
         {
             url: 'http://123.207.189.27:7138/RealTimeMonitorController/washEarn',
+            method: 'post',
+            data: param,
+            flag: 'out'
+        }
+    )
+}
+
+export function queryInRecord(param) {
+    return request(
+        {
+            url: 'http://192.168.1.163:8000/RealTimeMonitorController/getRecentCarInRecord',
             method: 'post',
             data: param,
             flag: 'out'
--- src/views/reportAnalysis/entranceExitMonitoring.vue
+++ src/views/reportAnalysis/entranceExitMonitoring.vue
@@ -1,115 +1,226 @@
-<!--
-    出入口监测
- * @Author:邵青阳
- * @Date: 2020-11-09 10:10:54
- * @LastEditTime: 2020-11-19 19:28:35
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \g524-comprehensive-displayd:\TingCar\src\views\reportAnalysis\entranceExitMonitoring.vue
--->
 <template>
   <div class="all">
-    <div class="up">
-      <!--上半部分查询-->
-      <el-form :inline="true" :model="upQueryList" class="demo-form-inline">
-        <!--      停车场-->
-        <el-form-item label="停车场:">
-          <el-select
-            size="small"
-            style="width: 160px"
-            v-model="queryParkId"
-            placeholder="请选择停车场"
-          >
-            <el-option
-              v-for="(item, index) in parkingLotList"
-              :label="item.name"
-              :value="item.code"
-              :key="index"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <!--    时间-->
-        <el-form-item label="选择时间:">
-          <el-date-picker
-            v-model="upQueryList.minDateTime"
-            type="datetime"
-            size="small"
-            style="width: 160px"
-            value-format="yyyy-MM-dd hh:mm:ss"
-            placeholder="请选择起始时间"
-          >
-          </el-date-picker>
-          <span>
+    <!--上半部分入口监测-->
+    <div class="up-half">
+      <el-row class="query">
+        <el-form :inline="true" :model="upQueryList" class="demo-form-inline">
+          <el-form-item label="入口监测"></el-form-item>
+          <el-form-item label="停车场:">
+            <el-select size="small"
+                       style="width: 160px"
+                       v-model="upQueryList.parkId"
+            >
+              <el-option label="全部" value=""/>
+              <el-option
+                  v-for="(item, index) in parkingLotList"
+                  :label="item.name"
+                  :value="item.code"
+                  :key="index"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <!--    时间-->
+          <el-form-item label="选择时间:">
+            <el-date-picker
+                v-model="upQueryList.minDateTime"
+                type="datetime"
+                size="small"
+                style="width: 160px"
+                value-format="yyyy-MM-dd hh:mm:ss"
+                placeholder="请选择起始时间"
+            >
+            </el-date-picker>
+            <span>
             ~
           </span>
-          <el-date-picker
-            v-model="upQueryList.maxDateTime"
-            type="datetime"
-            size="small"
-            style="width: 160px"
-            placeholder="请选择截止日期"
-            value-format="yyyy-MM-dd hh:mm:ss"
-          >
-          </el-date-picker>
-        </el-form-item>
-        <!--    车牌号-->
-        <el-form-item label="车牌号:">
-          <el-input
-            size="small"
-            style="width: 160px"
-            v-model="upQueryList.plateNum"
-            placeholder="请输入车牌号"
-          ></el-input>
-        </el-form-item>
-        <!--      查询按钮-->
-        <el-form-item>
-          <el-button type="primary" size="small" @click="query">查询</el-button>
-          <el-button type="primary" size="small" @click="resetQuery"
-            >重置</el-button
-          >
-        </el-form-item>
-      </el-form>
-    </div>
-    <!--下半部分图片-->
-    <el-row class="down-up">
-      <div class="span-text">入口数据</div>
-      <el-row>
-        <el-col style="height: 70%" :offset="1" :span="9">
-          <img :src="entranceImgUrl" class="show" />
-        </el-col>
-        <el-col :span="12">
-          <div class="index">
-            <div class="scroll">
-              <ul :style="{ top }" :class="{ transition: index != 0 }">
-                <li v-for="(item, index) in blackTextList">
-                  {{ item.text }} {{ item.carType }} {{ item.plateNum }}
-                </li>
-              </ul>
-            </div>
-          </div>
-        </el-col>
+            <el-date-picker
+                v-model="upQueryList.maxDateTime"
+                type="datetime"
+                size="small"
+                style="width: 160px"
+                placeholder="请选择截止日期"
+                value-format="yyyy-MM-dd hh:mm:ss"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <!--    车牌号-->
+          <el-form-item label="车牌号:">
+            <el-input
+                size="small"
+                style="width: 160px"
+                v-model="upQueryList.plateNumber"
+                placeholder="请输入车牌号"
+            ></el-input>
+          </el-form-item>
+          <!--      查询按钮-->
+          <el-form-item>
+            <el-button type="primary" size="small" @click="query">查询</el-button>
+            <el-button type="primary" size="small" @click="resetQuery"
+            >重置
+            </el-button
+            >
+          </el-form-item>
+        </el-form>
       </el-row>
-    </el-row>
-
-    <el-row class="down-down">
-      <el-row class="span-text">出口数据</el-row>
-      <el-row>
+      <el-row class="data-content">
         <el-col style="height: 70%" :offset="1" :span="9">
-          <img :src="exitImgUrl" class="show" />
+          <img :src="this.selectedInRecord.inImage" class="show"/>
         </el-col>
-        <el-col :span="12">
-          <div class="index">
-            <div class="scroll">
-              <ul :style="{ top }" :class="{ transition: index != 0 }">
-                <li v-for="(item, index) in blackTextList">
-                  {{ item.text }} {{ item.carType }} {{ item.plateNum }}
-                </li>
-              </ul>
-            </div>
+        <el-col :span="14">
+          <el-table :cell-style="{fontfamily: 'PingFangSC-Regular',letterSpacing: '0.56px',
+          fontSize: '14px',color: '#333333','text-align': 'center'}" :data="carInRecordList" :header-cell-style="{
+          fontfamily: 'PingFangSC-Medium',
+          background: '#FFFFFF',
+          color: '#333333',
+          border: 'none',
+          padding: 'none',
+          fontSize: '14px',
+          letterSpacing: '0.56px',
+          'text-align': 'center'}"
+                    stripe
+                    style="width: 100%;margin-left: 1%"
+                    @row-click = "handleSelection"
+                    v-show="inRecordShowDetial">
+            <el-table-column label="入场时间" prop="inTime" label-width="210px"></el-table-column>
+            <el-table-column
+                :show-overflow-tooltip="true"
+                label="车牌号"
+                prop="plateNumber"
+            />
+            <el-table-column
+                :show-overflow-tooltip="true"
+                label="停车场名称"
+                prop="parkName"
+            />
+            <el-table-column
+                :show-overflow-tooltip="true"
+                label="通过设备"
+                prop="inDeviceName"
+            />
+            <el-table-column :show-overflow-tooltip="true" label="操作">
+              <template slot-scope="scope">
+                <el-button
+                    size="small"
+                    type="text"
+                    @click="showDetail(scope.row)"
+                >查看详情
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div style="float: right;" v-show="inRecordShowDetial">
+            <el-pagination
+                layout="total, prev, pager, next, jumper"
+                :page-size="upPageSize"
+                @current-change="handleUpQuery"
+                :current-page="upPageNum"
+                :total="upTotal"
+                style="height: 10%"
+            >
+            </el-pagination>
           </div>
+          <el-row v-show="!inRecordShowDetial">
+            <el-row style="margin-top: 1%">
+              <el-col :span="7" offset="1"><span>停车场:{{ this.selectedInRecord.parkName }}</span></el-col>
+              <el-col :span="7" offset="1"><span>通过设备:{{ this.selectedInRecord.inDeviceName }}</span></el-col>
+              <el-col :span="8"><span>入场时间:{{ this.selectedInRecord.inTime }}</span></el-col>
+            </el-row>
+            <el-row style="margin-top: 8%">
+              <el-col :span="7" offset="1"><span>车牌号:{{ this.selectedInRecord.plateNumber }}</span></el-col>
+              <el-col :span="7" offset="1"><span>车牌颜色:{{ this.selectedInRecord.plateColor }}</span></el-col>
+              <el-col :span="8"><span>操作员:{{ this.selectedInRecord.stationOperator }}</span></el-col>
+            </el-row>
+            <el-row style="margin-top: 8%">
+              <el-col :span="15" offset="1"><span>备注:{{ this.selectedInRecord.remark }}</span></el-col>
+              <el-col :span="8">
+                <el-button type="primary" @click="inRecordShowDetial = true">返回</el-button>
+              </el-col>
+            </el-row>
+          </el-row>
         </el-col>
-      </el-row></el-row
-    >
+
+      </el-row>
+
+    </div>
+    <!--    下半部分出口检测-->
+    <div class="down-half">
+      <el-row class="query">
+        <el-form :inline="true" :model="upQueryList" class="demo-form-inline">
+          <el-form-item label="停车场:">
+            <el-select size="small"
+                       style="width: 160px"
+                       v-model="queryParkId">
+              <el-option label="全部" value=""/>
+              <el-option
+                  v-for="(item, index) in parkingLotList"
+                  :label="item.name"
+                  :value="item.code"
+                  :key="index"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <!--    时间-->
+          <el-form-item label="选择时间:">
+            <el-date-picker
+                v-model="upQueryList.minDateTime"
+                type="datetime"
+                size="small"
+                style="width: 160px"
+                value-format="yyyy-MM-dd hh:mm:ss"
+                placeholder="请选择起始时间"
+            >
+            </el-date-picker>
+            <span>
+            ~
+          </span>
+            <el-date-picker
+                v-model="upQueryList.maxDateTime"
+                type="datetime"
+                size="small"
+                style="width: 160px"
+                placeholder="请选择截止日期"
+                value-format="yyyy-MM-dd hh:mm:ss"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <!--    车牌号-->
+          <el-form-item label="车牌号:">
+            <el-input
+                size="small"
+                style="width: 160px"
+                v-model="upQueryList.plateNum"
+                placeholder="请输入车牌号"
+            ></el-input>
+          </el-form-item>
+          <!--      查询按钮-->
+          <el-form-item>
+            <el-button type="primary" size="small" @click="query">查询</el-button>
+            <el-button type="primary" size="small" @click="resetQuery"
+            >重置
+            </el-button
+            >
+          </el-form-item>
+        </el-form>
+      </el-row>
+      <el-row class="data-content">
+        <el-row>
+          <el-col style="height: 70%" :offset="1" :span="9">
+            <img :src="entranceImgUrl" class="show"/>
+          </el-col>
+          <el-col :span="12">
+            <div class="index">
+              <div class="scroll">
+                <ul :style="{ top }" :class="{ transition: index != 0 }">
+                  <li v-for="(item, index) in blackTextList">
+                    {{ item.text }} {{ item.carType }} {{ item.plateNum }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -128,80 +239,53 @@ export default {
       upQueryList: [],
       //时间选择器绑定
       value1: "",
-      //图片路径
-      entranceImgUrl: require("../../assets/images/cc1.png"),
-      exitImgUrl: require("../../assets/images/rc1.png"),
       //滚动效果
       animate: true,
-      //滚动数据暂存
-      blackTextList: [
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        },
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        },
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        },
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        },
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        },
-        {
-          text: "2020-08-07 17:50:40",
-          plateNum: "苏F-D01043",
-          carType: "小轿车"
-        }
-      ]
+      //入场记录
+      carInRecordList: [
+
+      ],
+      //查看具体数据
+      inRecordShowDetial: true,
+      //选中的记录
+      selectedInRecord: {},
+      upPageNum: 1,
+      upPageSize: 4,
+      upTotal: ''
     };
   },
   mounted() {
     this.queryParking();
-    this.goScroll();
+    this.query();
+
   },
   methods: {
-    goScroll() {
-      var _this = this;
-      this.p = setInterval(() => {
-        console.log(22);
-        _this.top = -60 * _this.index + "px";
-        if (_this.index >= this.blackTextList.length + 1) {
-          _this.index = 0;
-          _this.top = -0 + "px";
-          clearInterval(_this.p);
-          _this.continueScroll();
-        } else {
-          _this.index++;
-        }
-      }, 1000);
+    handleSelection(row){
+      this.selectedInRecord = row;
     },
-    continueScroll() {
-      var _this = this;
-      setTimeout(() => {
-        _this.index = 1;
-        _this.top = -60 * this.index + "px";
-        _this.index++;
-        this.goScroll();
-      }, 100);
+    handleUpQuery(val) {
+      this.upPageNum = val;
+      this.query();
     },
     //查询重置按钮
     resetQuery() {
       this.upQueryList = {};
     },
-    query() {},
+    query() {
+      const param = {
+        pageNum: this.upPageNum,
+        pageSize: this.upPageSize,
+        parkId: this.upQueryList.parkId,
+        plateNumber: this.upQueryList.plateNumber,
+        startTime: this.upQueryList.minDateTime,
+        endTime: this.upQueryList.maxDateTime
+      };
+      this.$realTimeMonitor.queryInRecord(param).then(res => {
+        this.carInRecordList = res.resultEntity.list;
+        this.upTotal =  res.resultEntity.total;
+        this.selectedInRecord = this.carInRecordList[0];
+      });
+    },
     // 查询停车场下拉表单
     queryParking() {
       var that = this;
@@ -216,7 +300,11 @@ export default {
         that.parkingLotList = res.data.dataList;
         console.log("下拉菜单", this.parkingLotList);
       });
-    }
+    },
+    showDetail(item) {
+      this.selectedInRecord = item;
+      this.inRecordShowDetial = false;
+    },
   },
   destroyed() {
     clearInterval(this.p);
@@ -231,6 +319,38 @@ export default {
   overflow: hidden;
 }
 
+.up-half {
+  width: 98%;
+  height: 48%;
+  background-color: white;
+  margin-left: 1%;
+  margin-top: 0.5%;
+}
+
+.down-half {
+  width: 98%;
+  height: 48%;
+  background-color: white;
+  margin-left: 1%;
+  margin-top: 0.5%;
+}
+
+.query {
+  width: 98%;
+  height: 10%;
+  background-color: white;
+  margin-left: 1%;
+  margin-top: 0.5%;
+}
+
+.data-content {
+  width: 98%;
+  height: 39%;
+  background-color: white;
+  margin-left: 1%;
+  margin-top: 1%;
+}
+
 /*查询*/
 .up {
   width: 98%;
@@ -239,6 +359,7 @@ export default {
   margin-left: 1%;
   margin-top: 0.5%;
 }
+
 /* 查询条件部分样式 */
 .demo-form-inline {
   width: 100%;
@@ -246,8 +367,9 @@ export default {
   padding-left: 1%;
   padding-top: 0.5%;
 }
+
 .show {
-  height: 280px;
+  height: 300px;
   width: 80%;
   padding-left: 18px;
   position: relative;
@@ -261,6 +383,7 @@ export default {
   margin-left: 1%;
   margin-top: 1%;
 }
+
 .down-down {
   width: 98%;
   height: 42%;
@@ -268,6 +391,7 @@ export default {
   margin-left: 1%;
   margin-top: 1%;
 }
+
 .span-text {
   font-size: 20px;
   width: 100%;
@@ -285,6 +409,7 @@ export default {
 /deep/ .el-table .successSecond {
   background: #8ed3e7 !important;
 }
+
 img {
   width: 30px;
   height: 30px;
@@ -292,9 +417,11 @@ img {
   vertical-align: middle;
   margin-right: 20px;
 }
+
 ul {
   position: relative;
 }
+
 li {
   overflow: hidden;
   white-space: nowrap;
@@ -306,12 +433,14 @@ li {
   margin: 0;
   font-size: 14px;
 }
+
 .scroll {
   height: 60px;
   overflow: hidden;
   font-size: 0px;
   position: relative;
 }
+
 .transition {
   transition: top 1s;
 }
