b9ba82d302ff1a964e5a6a177521bc1cf8942da6
小呆同学 <2315741635@qq.com>
2021-05-17 11:20:00 +0800

    2021-05-17

--- src/axios/listManagement/listManagement.js
+++ src/axios/listManagement/listManagement.js
@@ -3,19 +3,19 @@ import request from "@/utils/request";
 //查询专车列表
 export function specialParkWhiteList(param) {
   return request({
-    url: "http://192.168.1.63:8000/AuthorizationController/specialParkWhiteList",
+    url: "/AuthorizationController/specialParkWhiteList",
     method: "post",
     data: param,
-    flag:"out"
+
   });
 }
 //专车配置修改
 export function updateSpecialParkWhiteList(param) {
   return request({
-    url: "http://192.168.1.63:8000/AuthorizationController/updateSpecialParkWhiteList",
+    url: "/AuthorizationController/updateSpecialParkWhiteList",
     method: "post",
     data: param,
-    flag:"out"
+
   });
 }
 
